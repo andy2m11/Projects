a29b24a308ba91896dc83a048b0ab1947dfb49f5
Andy Mo <andy2m11@gmail.com>
2014-02-03 18:51:23 -0500

    added some server files.

---
+++ LT Server Unix/LT Server Console U/fifoDataIn.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+#include <queue>
+
+
+void datain(void)
+{
+	
+	char dat[1000];
+	char msgtype;
+	char addr;
+
+	char buffer[1002];
+
+	buffer[0] = addr;	
+	buffer[1] = msgtype;
+
+
+	fifoQ.push(buffer);
+
+	
+
+}
